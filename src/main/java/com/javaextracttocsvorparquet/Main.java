0a90037ac7e61733ef32af99aba92c3e393ca80f
Soap1196 <Jdmack007@aol.com>
2025-09-12 14:47:37 -0500

    inital commit

---
+++ src/main/java/com/javaextracttocsvorparquet/Main.java
@@ -0,0 +1,7 @@
+package com.javaextracttocsvorparquet;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/com/javaextracttocsvorparquet/manager/CarManager.java
@@ -0,0 +1,16 @@
+package main.java.com.javaextracttocsvorparquet.manager;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import models.cars;
+
+public class CarManager {
+
+    public ArrayList<cars> getCarList() {
+        ArrayList<cars> carList = new ArrayList<>();
+    }
+    
+}
